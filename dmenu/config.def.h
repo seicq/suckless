45f14520d103790922a7e689acfeeb561fe20c65
HarshWardhan Kumar <harshwardhankumar18@gmail.com>
2024-08-11 01:29:26 +0530

    Adding Files

---
+++ dmenu/config.def.h
@@ -0,0 +1,25 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"JetBrains Mono NF:size=16"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+#include "/home/harsh/.cache/wal/colors-wal-dmenu.h"
+
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 10;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 2;
